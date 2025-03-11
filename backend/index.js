4f99968821f004cd6cef988a43f28bfdac074ade
Cakwei <charleetan2020@gmail.com>
2025-03-11 23:44:30 +0800

    wegfwefgw

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const mysql = require("mysql2");
 
-app.use(cors())
+app.use(cors());
+
 app.listen(8080, () => {
-      console.log('server listening on port 8080')
-})
-app.get('/', (req, res) => {
-    res.send('Hello from our servers!')
-})
+  console.log("server listening on port 8080");
+});
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO user (username, userpass) VALUES ('Cakwei', '123')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,51 @@
-import axios from "axios";
 import { getDefaultMode } from "../constants/index.js";
-import '../styles/Login.css';
+import { useState } from "react";
+import "../styles/Login.css";
 export default function Login() {
-
-  const apiCall = () => {
-    axios.get('http://localhost:8080').then((data) => {
-      //this console.log will be in our frontend console
-      console.log(data)
-    })
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    setName("");
   }
-  
   return (
     <div
-      className={`${getDefaultMode() ? "dark" : "light"} text-black dark:text-white bg-white dark:bg-black`}
+      className={`${getDefaultMode() ? "dark" : "light"} flex h-dvh min-h-[250px] w-dvw min-w-[250px] items-center justify-center overflow-scroll bg-yellow-300 text-black dark:bg-black dark:text-white`}
     >
-      <title>Account Sign In</title>
-        <button className="btn" onClick={apiCall}>Make API Call</button>
+      <title>Log In</title>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-[min(80%,550px)] flex-col items-center"
+      >
+        <label>Username: </label>
+
+        <input
+          id="username"
+          className="input"
+          value={name}
+          name="username"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+
+        <br />
+        <label>Password: </label>
+
+        <input
+          id="password"
+          className="input"
+          value={pass}
+          name="password"
+          onChange={(e) => {
+            setPass(e.target.value);
+          }}
+        ></input>
+
+        <button className="btn btn-success" type="submit">
+          Submit form
+        </button>
+      </form>
     </div>
   );
 }
