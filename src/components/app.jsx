43de833018c10371b118391fa1d4a3a541e790b0
Cakwei <charleetan2020@gmail.com>
2025-03-13 17:08:20 +0800

    refw3ef

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Home from "../pages/Home.jsx";
 import Login from "../pages/Login.jsx";
 import Contact from "../pages/Contact.jsx";
-import Rewards from "../pages/rewards.jsx";
+import Rewards from "../pages/Rewards.jsx";
 import Location from "../pages/Locations.jsx";
 import { BrowserRouter, HashRouter, Route, Routes } from "react-router";
 export default function App() {
